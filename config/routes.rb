b0717c0973b16eef6f4755dc7179fd5b8c1e345c
Reayah Bravo <reayahbravo@gmail.com>
2018-06-27 13:01:04 -0700

    added welcome and about contact controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get('/', { to: 'welcome#index', as: 'home' })
+  get('/about', { to: 'about#index' })
+  get('/contact', { to: 'contact#index' })
+  post('/contact_submit', { to: 'contact#create' })
 end
